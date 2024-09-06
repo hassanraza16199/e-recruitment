cc358aa1ea902309cbf77cd2bd606dec0b1a371b
hussnain <118718287+web0development@users.noreply.github.com>
2024-09-06 19:43:09 +0500

    fix the title of page

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include "connection.php";
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-         <title>E-Recruitment system</title>
+         <title>E-Recruitment</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="manifest" href="site.webmanifest">
