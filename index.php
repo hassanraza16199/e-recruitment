7a52c2d8ad07b6724749f99bd4580c214e179c51
Hassan Raza <122777148+HassanRaza572@users.noreply.github.com>
2025-07-08 16:32:44 +0500

    Bug 11

--- index.php
+++ index.php
@@ -321,7 +321,7 @@ form.search-box .select-form .nice-select::after {
                                                 $b = 0;
                                                 $num = $a + $b;
                                             ?>
-                                            <li><a href="index.php">Home <?php echo $numqz; ?></a></li>
+                                            <li><a href="index.php">Home <?php echo $numzq; ?></a></li>
                                             
                                             <li><a href="#aboutus">About us</a></li>
                                             
