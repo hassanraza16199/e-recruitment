121950be2ea2575aca50678e1c817da722118f6b
Hassan Raza <122777148+HassanRaza572@users.noreply.github.com>
2025-07-14 18:54:36 +0500

    Fixed Error

--- index.php
+++ index.php
@@ -594,7 +594,7 @@ form.search-box .select-form .nice-select::after {
                     <div class="col-xl-10">
                         <!-- single-job-content -->
                          <?php
-                        include "connection.php";
+                        // Use the already included connectionnn.php at the top for $conn
                         $sql = "SELECT * FROM job_post LIMIT 5";
                         $result = $conn->query($sql);
                         if ($result->num_rows > 0) {
@@ -828,12 +828,11 @@ form.search-box .select-form .nice-select::after {
                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
                     <div class="footer-tittle-bottom">
                     <?php
-                                            include 'connection.php';
+                                            // Use the already included connectionnn.php at the top for $conn
                                             $count_sql = "SELECT COUNT(*) AS total FROM user";
                                             $count_result = $conn->query($count_sql);
                                             $count_row = $count_result->fetch_assoc();
                                             $total_user_entries = $count_row['total'];
-                                            $conn->close();
                                             ?>
                         <span><?php echo $total_user_entries; ?> +</span>
                         <p>Talented Hunter</p>
@@ -841,12 +840,11 @@ form.search-box .select-form .nice-select::after {
                     </div>
                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
                     <?php
-                                            include 'connection.php';
+                                            // Use the already included connectionnn.php at the top for $conn
                                             $count_sql = "SELECT COUNT(*) AS total FROM job_post";
                                             $count_result = $conn->query($count_sql);
                                             $count_row = $count_result->fetch_assoc();
                                             $total_entries = $count_row['total'];
-                                            $conn->close();
                                             ?>
                         <div class="footer-tittle-bottom">
                             <span><?php echo $total_entries; ?> +</span>
@@ -855,12 +853,11 @@ form.search-box .select-form .nice-select::after {
                     </div>
                     <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
                     <?php
-                                            include 'connection.php';
+                                            // Use the already included connectionnn.php at the top for $conn
                                             $count_sql = "SELECT COUNT(*) AS total FROM applications";
                                             $count_result = $conn->query($count_sql);
                                             $count_row = $count_result->fetch_assoc();
                                             $total_application_entries = $count_row['total'];
-                                            $conn->close();
                                             ?>
                         <!-- Footer Bottom Tittle -->
                         <div class="footer-tittle-bottom">
